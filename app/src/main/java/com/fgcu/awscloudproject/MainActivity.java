382f798bc7f817bbfe410974507761edf0e64d2b
Edgar Meruvia <emeruvia@gmail.com>
2018-04-20 02:29:40 -0400

    progress so far it includes can take a picture and set a thumbnail, need to implement saving a picture and uploading to s3. "https://developer.android.com/training/camera/photobasics.html#TaskPath"

--- app/src/main/java/com/fgcu/awscloudproject/MainActivity.java
+++ app/src/main/java/com/fgcu/awscloudproject/MainActivity.java
@@ -1,19 +1,39 @@
 package com.fgcu.awscloudproject;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.AWSStartupHandler;
 import com.amazonaws.mobile.client.AWSStartupResult;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView pictureImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pictureImageView = findViewById(R.id.pictureImageView);
 
         //Connect Your Back-end part of the setup AWS
         AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
@@ -22,5 +42,26 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
             }
         }).execute();
+    }
+
+    /**
+     * When the user clicks on the take picture onclick it will take a picture with the camera
+     *
+     * @param view
+     */
+    public void takePictureOnClick(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            pictureImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
