57ce713c8f473c759b74090c05acdce9b988886f
Edgar Meruvia <emeruvia@gmail.com>
2018-04-20 00:55:00 -0400

    Successfully connect to cloud

---
+++ app/src/main/java/com/fgcu/awscloudproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.fgcu.awscloudproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Connect Your Back-end part of the setup AWS
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
+    }
+}
